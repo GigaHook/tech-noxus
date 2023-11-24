940e975172fd5f5b1a67c2f16926183b584df235
Smirka <smirka.n.u@gmail.com>
2023-11-25 05:38:59 +0700

    titles remake

--- src/components/Title.vue
+++ src/components/Title.vue
@@ -1,14 +1,21 @@
 <template>
-  <h2 class="text-h4 px-2 pt-1 mb-2 title">
-    # <slot/>
+  <h2 class="text-h4 pa-2 px-4 title">
+    <div class="slot">
+      # <slot/>
+    </div>
   </h2>
 </template>
 
 <style scoped>
 .title {
-  background-color: #FFC400;
-  width: fit-content;
-  text-shadow: 5px -5px white;
   opacity: 0;
+  width: fit-content;
+  background-color: #ffc400;
+  color: white;
+  transform: skewX(-45deg);
+}
+
+.slot {
+  transform: skewX(45deg);
 }
 </style>
