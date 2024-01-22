4655a329f588d7f2712ec80fa178446be6c3bc97
Smirka <smirka.n.u@gmail.com>
2024-01-23 01:49:17 +0700

    storage wtfff

--- src/composables/api.js
+++ src/composables/api.js
@@ -1,8 +1,47 @@
-import { useStorage } from "@vueuse/core"
-import { useAxios } from '@vueuse/integrations/useAxios'
+import { useFetch, useStorage } from "@vueuse/core"
+import { useCookies } from "@vueuse/integrations/useCookies"
+import axios from 'axios'
+
+const user = useStorage('user')
+const apiToken = useStorage('api-token')
+const cookies = useCookies()
+
+axios.defaults.withCredentials = true
+axios.defaults.withXSRFToken = true
+
+export function useAuth() {
+  async function login(formData) {
+    await axios.get(import.meta.env.VITE_API_URL + '/sanctum/csrf-cookie')
+    const response = await axios.post(import.meta.env.VITE_API_URL + '/login', {
+      login: formData.value.login,
+      password: formData.value.password,
+    })
+
+    user.value = JSON.stringify(response.data.user)
+    apiToken.value = response.data.token
+  }
+
+  async function logout() {
+    await axios.get(import.meta.env.VITE_API_URL + '/logout')
+    user.value
+    apiToken.value
+    cookies.remove('XSRF-TOKEN')
+    console.log(user.value)
+  }
+
+  async function register() {
+
+  }
+
+  return {
+    login,
+    logout,
+    register,
+  }
+}
 
 export function useApi() {
   return {
-
+    
   }
 }
--- src/pages/Auth.vue
+++ src/pages/Auth.vue
@@ -11,16 +11,15 @@
             <v-form
               ref="form"
               @submit.prevent="submit"
-              
             >
               <v-text-field
-                v-model="login"
+                v-model="formData.login"
                 label="Логин"
                 :rules="[rules.required, rules.range]"
               />
 
               <v-text-field
-                v-model="password"
+                v-model="formData.password"
                 label="Пароль"
                 :rules="[rules.required, rules.range]"
                 type="password"
@@ -32,20 +31,15 @@
               />
             </v-form>
           </v-card-text>
-
-          <v-card-actions>
-            <v-btn @click="getToken">
-              get token
-            </v-btn>
-          </v-card-actions>
         </v-card>
 
         <v-card class="mt-2" v-if="user">
           <v-card-text>
             {{ user }}
           </v-card-text>
+
           <v-card-actions>
-            <v-btn @click="logout">
+            <v-btn @click="asd">
               Выйти
             </v-btn>
           </v-card-actions>
@@ -56,10 +50,9 @@
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { ref, watch, reactive } from 'vue'
 import { useFetch, useStorage } from '@vueuse/core'
-import { useCookies } from '@vueuse/integrations/useCookies'
-import axios from 'axios'
+import { useAuth } from '@/composables/api'
 
 const rules = {
   required: v => !!v || 'Это поле нужно заполнить',
@@ -67,92 +60,31 @@ const rules = {
 }
 
 const form = ref()
-const login = ref()
-const password = ref()
-const apiToken = useStorage('api-token')
-const user = ref()
+const formData = ref({
+  login: '',
+  password: '',
+})
 
-const { execute: getUser } = useFetch(import.meta.env.VITE_API_URL + '/user', {
-  immediate: false,
-  beforeFetch({ options }) {
-    options.headers = {
-      ...options.headers,
-      'Authorization': `Bearer ${apiToken.value}`,
-    }
-  },
-  afterFetch(ctx) {
-    console.log(ctx.data)
-    user.value = ctx.data
+const user = useStorage('user')
+const { login, logout } = useAuth()
+
+async function submit() {
+  await form.value.validate()
+
+  if (!form.value.isValid) return
+
+  try {
+    await login(formData)
+  } catch (error) {
+
   }
-})
+}
 
-//getUser()
-
-const { execute: getToken } = useFetch(import.meta.env.VITE_API_URL + '/sanctum/csrf-cookie', {
-  immediate: false,
-  credentials: 'include',
-})
-
-const { execute: logout } = useFetch(import.meta.env.VITE_API_URL + '/logout', {
-  immediate: false,
-  beforeFetch({ options }) {
-    options.headers = {
-      ...options.headers,
-      'Authorization': `Bearer ${apiToken.value}`,
-    }
-  },
-  afterFetch() {
-    user.value = null
-    //apiToken.value = ''
-  }
-})
-
-//axios.defaults.withCredentials = true
-//axios.defaults.withXSRFToken = true
-//
-//axios.get(import.meta.env.VITE_API_URL + '/sanctum/csrf-cookie')
-
-//TODO пофиксить этот ад
-function submit() {
-  form.value.validate().then(async () => {
-    if (form.value.isValid) {
-      await useFetch(import.meta.env.VITE_API_URL + '/sanctum/csrf-cookie', {
-        credentials: 'include',
-      })
-
-      const cookies = useCookies(["XSRF-TOKEN"])
-
-      await useFetch(import.meta.env.VITE_API_URL + '/login', {
-        credentials: "include",
-        body: {
-          login: login.value,
-          password: password.value,
-        },
-        beforeFetch({ options }) {
-          options.headers = {
-            ...options.headers,
-            "X-XSRF-TOKEN": cookies.get("XSRF-TOKEN"),
-          }
-        },
-      }).post()
-      //axios.get(import.meta.env.VITE_API_URL + '/sanctum/csrf-cookie').then(response => {
-      //  console.log(response)
-      //  axios.post(import.meta.env.VITE_API_URL + '/login', {
-      //    login: login.value,
-      //    password: password.value,
-      //  }).then(response => {
-      //    apiToken.value = response.data['token']
-      //    console.log(apiToken.value)
-      //    getUser()
-      //    axios.get(import.meta.env.VITE_API_URL + '/user').then(response => {
-      //      console.log(response.data)
-      //    })
-      //  }).catch(error => {
-      //    console.log(error)
-      //  })
-      //})
-    }
-  })
+async function asd() {
+  await logout()
+  console.log(user.value)
+  const user2 = useStorage('user')
+  console.log(user2.value)
 }
 
 </script>
